4ae1d392704fb4dee41b17c3948032e4e5aa797e
gerardoluceroc <gerardo.lucero.c@usach.cl>
2024-07-27 15:53:24 -0400

    Listo para trabajar en home page
    Se hicieron los ajustes necesarios para trabajar con react-router-dom

--- frontend-react/src/App.jsx
+++ frontend-react/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path='/home' element={<HomePage/>}/>
+        <Route path='/' element={<HomePage/>}/>
       </Routes>
     </>
   )
